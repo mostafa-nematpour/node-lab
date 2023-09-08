b440bad95c5415dfa8d8f41881dfeb1e257bdae4
mostafa nematpour <nematpor46@gmail.com>
2023-09-08 14:04:40 +0330

    add validator

--- 4-nest/1-codingthesmartway.com-project/src/courses/dto/create-course.dto.ts
+++ 4-nest/1-codingthesmartway.com-project/src/courses/dto/create-course.dto.ts
@@ -1,7 +1,19 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateCourseDto {
+  @IsNotEmpty()
+  @IsNumber()
   readonly id: number;
+
+  @IsNotEmpty()
   readonly title: string;
+
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNotEmpty()
   readonly author: string;
+
+  @IsNotEmpty()
   readonly url: string;
 }
