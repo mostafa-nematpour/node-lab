4df49e777940ce44ae21b87ba1863c6147cc888a
mostafa nematpour <nematpor46@gmail.com>
2023-10-10 14:53:13 +0330

    init auth-module oauth2.0

--- 4-nest/oauth2.0/src/users/users.service.ts
+++ 4-nest/oauth2.0/src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'user1',
+      password: 'pass1',
+      tokens: [],
+    },
+    {
+      userId: 2,
+      username: 'user2',
+      password: 'pass2',
+      tokens: [],
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
